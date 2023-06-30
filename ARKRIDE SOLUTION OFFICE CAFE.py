cac3c284cefe97388448a6ffc1d085b06eb02bf9
HadzranAziis22 <137128804+HadzranAziis22@users.noreply.github.com>
2023-06-30 09:30:50 +0700

    webdevelopment

---
+++ ARKRIDE SOLUTION OFFICE CAFE.py
@@ -0,0 +1,81 @@
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial scale=1.0"/>
+        <meta http-equiv="X-UA-compatible" content="ie=edge"/>
+        <title>ARKRIDE SOLUTION OFFICE</title>
+    </head>
+    <body>
+        <h1>ARKRIDE SOLUTION OFFICE Cafe</h1>
+        <P>Selamat datang di website ARKRIDE SOLUTION OFFICE, Silahkan mengeksplor website ini.</P>
+        <h2>Request menu</h2>
+        <table> Minuman dan Es
+            <tr>
+                <th>&nbsp;</th>
+                <th>Panas (P)</th>
+                <th>Dingin (D)</th>
+                <th>mixer (M)</th>
+            <tr>
+            <tr>
+                <td>Kopi hitam</td>
+                <td>2.000</td>
+                <td>4.000</td>
+                <td>1.000</td>
+             </tr>
+             <tr>
+                <td>es serut</td>
+                <td>-</td>
+                <td>8.000</td>
+                <td>10.000</td>
+             </tr>  
+             <tr>
+                <td> es teler</td>
+                <td>-</td>
+                <td>9.000</td>
+                <td>15.000</td>
+                <tr>
+                    <td>Air Mineral</td>
+                    <td>500</td>
+                    <td>2.000</td>
+                    <td>-</td>
+                </tr>
+             </tr>  
+         </table>
+         <h2>menu Daging Steak</h2>
+         <table> Tenderloin version (T) , Siroin version (S) dan Well done version (W)
+             <tr>
+                <th>&nbsp;</th>
+                <th>Tenderloin (T)</th>
+                <th>Siroin (S)</th>
+                <th>well done (w)</th>
+             </tr> 
+            <tr>
+                <td>BBQ chicken</td>
+                <td>40.000(T)</td>
+                <td>30.000(S)</td>
+                <td>50.000(W)</td>
+             <tr>
+                <td>BBQ Meat</td>
+                <td>30.000(T)</td>
+                <td>40.000(S)</td>
+                <Td>60.000(W)</Td>
+             </tr>    
+             <tr>
+                <td> BBQ Duck</td>
+                <td>20.000(T)</td>
+                <td>40.000(S)</td>
+                <td>70.000(W)</td>
+             </tr>
+            </tr>
+         </table>
+         <h2>Tentang kami</h2>
+         <p>Hubungi kami melalui
+            <a href="vdonalaggon@gmail.com">Email</a>
+            ,
+            <a href="https://www.instagram.com/?hl=id">Instagram</a>
+            dan
+            <a href ="https://www.facebook.com/profile.php?id=100079204021712">Facebook</a>
+            .
+         </p>
+    </body>
+</html>
